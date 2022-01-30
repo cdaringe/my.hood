18b7bcdb2b8c9e8b5a65fc05cb4d997c813b2039
cdaringe <cdaringe@gmail.com>
2022-01-30 12:08:31 -0800

    fix: add public name

--- rad.ts
+++ rad.ts
@@ -6,7 +6,7 @@ const watch: Task = `${build} -w`;
 const test: Task = `dune test`;
 // OSX & openssl are grumpy together, as osx ships some old version, but ocaml libs need the good stuff
 // PKG_CONFIG_PATH="$(brew --prefix openssl)/lib/pkgconfig:$PKG_CONFIG_PATH"
-const install: Task = `opam install dune && opam install . -y --deps-only --with-doc --with-test --locked && dune build && opam install . --deps-onl`;
+const install: Task = `opam install dune && opam install . -y --deps-only --with-doc --with-test --locked && dune build && opam install . --deps-only`;
 const exportSwitch: Task = `opam switch export ./switch`;
 const format: Task = `opam exec -- dune build @fmt --auto-promote`;
 const site: Task = `npx concurrently --raw "npx parcel serve src/bin/site/index.html" "dune build -w"`;
